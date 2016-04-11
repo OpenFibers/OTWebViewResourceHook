34afef1d694ccd5c6f43c49688ed5b858bbf36ec
OpenThread <openfibers@gmail.com>
2016-04-11 19:04:26 +0800

    Initial Commit

---
+++ OTWebViewResourceHookDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OTWebViewResourceHookDemo
+//
+//  Created by openthread on 4/11/16.
+//  Copyright © 2016 openthread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
